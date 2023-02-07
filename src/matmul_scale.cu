0e32507bf44d4b638a84d3c3769b40698dd1dd48
chuchaoqun <chuchaoqun@megvii.com>
2023-02-07 16:25:49 +0800

    simplify the code

--- src/matmul_scale.cu
+++ src/matmul_scale.cu
@@ -1,10 +1,9 @@
-#include "./matmul_scale.h"
+#include "megbrain/custom/custom.h"
 #include "megbrain/custom/platform/custom_cuda.h"
 #include <cuda.h>
 #include <cuda_runtime.h>
-#include <stdio.h>
 
-using namespace custom;
+/****************** My Kernels ******************/
 
 // matmul_forward for Mat_mxk * Mat_k*n
 template <typename T>
@@ -45,45 +44,74 @@ __global__ void matmul_backward_rhs_naive(const T *lhs, const T *ograd, T *rhs_g
     rhs_grad[row * N + col] = acc / scale;
 }
 
+/****************** wrap kernel by custom op ******************/
+
+// using custom::Shape;
+// using custom::Param;
+// using custom::Tensor;
+using namespace custom;
+
+void forward_shape_infer(const std::vector<Shape> &inputs, const Param &params,
+                         std::vector<Shape> &outputs) {
+    outputs[0] = {inputs[0][0], inputs[1][1]};
+}
+
 void matmul_forward(const Tensor &lhs, const Tensor &rhs, Tensor &res, size_t M,
-                    size_t K, size_t N, float scale) {
+                    size_t K, size_t N, float scale) {}
+
+void forward_compute(const std::vector<Tensor> &inputs, const Param &params,
+                     std::vector<Tensor> &outputs) {
+    const Tensor &lhs = inputs[0];
+    const Tensor &rhs = inputs[1];
+    const Tensor &res = outputs[0];
+    size_t M = lhs.shape()[0], K = lhs.shape()[1], N = rhs.shape()[1];
+    float scale = params["scale"].as<float>();
+
     // get input cuda stream, and launch kernel on this stream
     auto stream = get_cuda_stream(lhs.device());
     dim3 block(1, 1);
     dim3 grid(N / block.x, M / block.y);
-
-    DISPATCH_INT_AND_FLOAT_TYPES(res.dtype(), "matmul_forward", ([&]() {
-                                     matmul_forward_naive<scalar_t>
-                                         <<<grid, block, 0, stream>>>(
-                                             lhs.data<scalar_t>(), rhs.data<scalar_t>(),
-                                             res.data<scalar_t>(), M, K, N, scale);
-                                 }));
+    matmul_forward_naive<float><<<grid, block, 0, stream>>>(
+        lhs.data<float>(), rhs.data<float>(), res.data<float>(), M, K, N, scale);
 }
 
-void matmul_backward_lhs(const Tensor &rhs, const Tensor &ograd, Tensor &lhs_grad,
-                         size_t M, size_t K, size_t N, float scale) {
-    // get input cuda stream, and launch kernel on this stream
-    auto stream = get_cuda_stream(rhs.device());
-    dim3 block(1, 1);
-    dim3 grid(K / block.x, M / block.y);
-    DISPATCH_INT_AND_FLOAT_TYPES(
-        lhs_grad.dtype(), "matmul_backward_lhs", ([&]() {
-            matmul_backward_lhs_naive<scalar_t><<<grid, block, 0, stream>>>(
-                rhs.data<scalar_t>(), ograd.data<scalar_t>(), lhs_grad.data<scalar_t>(),
-                M, K, N, scale);
-        }));
+CUSTOM_OP_REG(MatMulScaleForward)
+    .add_inputs(2)  // lhs, rhs
+    .add_outputs(1) // output
+    .add_param("scale", 1.0f)
+    .set_shape_infer(forward_shape_infer)
+    .set_compute("cuda", forward_compute);
+
+void backward_shape_infer(const std::vector<Shape> &ograd_and_inputs,
+                          const Param &params, std::vector<Shape> &outputs) {
+    outputs[0] = ograd_and_inputs[1];
+    outputs[1] = ograd_and_inputs[2];
 }
 
-void matmul_backward_rhs(const Tensor &lhs, const Tensor &ograd, Tensor &rhs_grad,
-                         size_t M, size_t K, size_t N, float scale) {
+void backward_compute(const std::vector<Tensor> &ograd_and_inputs, const Param &params,
+                      std::vector<Tensor> &igrads) {
+    const Tensor &ograd = ograd_and_inputs[0];
+    const Tensor &lhs = ograd_and_inputs[1];
+    const Tensor &rhs = ograd_and_inputs[2];
+    Tensor &lhs_grad = igrads[0], &rhs_grad = igrads[1];
+
+    size_t M = lhs.shape()[0], K = lhs.shape()[1], N = rhs.shape()[1];
+    float scale = params["scale"].as<float>();
+
     // get input cuda stream, and launch kernel on this stream
     auto stream = get_cuda_stream(lhs.device());
     dim3 block(1, 1);
-    dim3 grid(N / block.x, K / block.y);
-    DISPATCH_INT_AND_FLOAT_TYPES(
-        rhs_grad.dtype(), "matmul_backward_rhs", ([&]() {
-            matmul_backward_rhs_naive<scalar_t><<<grid, block, 0, stream>>>(
-                lhs.data<scalar_t>(), ograd.data<scalar_t>(), rhs_grad.data<scalar_t>(),
-                M, K, N, scale);
-        }));
+    dim3 grid_lhs(K / block.x, M / block.y);
+    dim3 grid_rhs(N / block.x, K / block.y);
+    matmul_backward_lhs_naive<float><<<grid_lhs, block, 0, stream>>>(
+        rhs.data<float>(), ograd.data<float>(), lhs_grad.data<float>(), M, K, N, scale);
+    matmul_backward_rhs_naive<float><<<grid_rhs, block, 0, stream>>>(
+        lhs.data<float>(), ograd.data<float>(), rhs_grad.data<float>(), M, K, N, scale);
 }
+
+CUSTOM_OP_REG(MatMulScaleBackward)
+    .add_inputs(3)  // ograd, lhs, rhs
+    .add_outputs(2) // lhs_grad, rhs_grad
+    .add_param("scale", 1.0f)
+    .set_shape_infer(backward_shape_infer)
+    .set_compute("cuda", backward_compute);
--- test.py
+++ test.py
@@ -1,38 +1,67 @@
+def run_forward():
+    from megengine.core._imperative_rt.core2 import apply
+    from megengine.core.ops import custom
+    from megengine.utils import custom_op_tools
 
-import numpy as np
+    lib_path = custom_op_tools.build_and_load(
+        "matmul_scale",
+        ["src/matmul_scale.cu"],
+        build_dir="build"
+    )
 
-from megengine.autodiff import Function, GradManager
-from megengine.core._imperative_rt.core2 import apply
-from megengine.core.ops import custom
-from megengine.tensor import Tensor
-from megengine.utils import custom_op_tools
-
-lib_path = custom_op_tools.build_and_load(
-    "matmul_scale",
-    ["src/matmul_scale.cpp", "src/matmul_scale.cu"],
-    build_dir="build"
-)
-
-class MatMulScaleFunc(Function):
-    def __init__(self, scale):
-        super().__init__()
-        self.scale = scale
-
-    def forward(self, lhs, rhs):
-        self.lhs = lhs
-        self.rhs = rhs
-        op = custom.MatMulScaleForward(scale=self.scale)
+    def matmul_scale(lhs, rhs, scale):
+        op = custom.MatMulScaleForward(scale=scale)
         return apply(op, lhs, rhs)
 
-    def backward(self, ograd):
-        op = custom.MatMulScaleBackward(scale=self.scale)
-        return apply(op, ograd, self.lhs, self.rhs)
+    import numpy as np
+    from megengine.tensor import Tensor
 
-matmul = MatMulScaleFunc(scale=0.1)
-lhs = Tensor(np.random.randn(2, 4))
-rhs = Tensor(np.random.randn(4, 8))
-grad = Tensor(np.random.randn(2, 8))
+    lhs = Tensor(np.random.randn(2, 4))
+    rhs = Tensor(np.random.randn(4, 8))
+    res = matmul_scale(lhs, rhs, 0.1)
 
-with GradManager().attach([lhs, rhs]) as gm:
-    (y,) = matmul(lhs, rhs)
-    gm.backward(y)
+def run_forward_and_backward():        
+    import numpy as np
+    from megengine.autodiff import Function, GradManager
+    from megengine.core._imperative_rt.core2 import apply
+    from megengine.core.ops import custom
+    from megengine.tensor import Tensor
+    from megengine.utils import custom_op_tools
+
+    lib_path = custom_op_tools.build_and_load(
+        "matmul_scale",
+        ["src/matmul_scale.cu"],
+        build_dir="build"
+    )
+
+    class MatMulScaleFunc(Function):
+        def __init__(self, scale):
+            super().__init__()
+            self.scale = scale
+
+        def forward(self, lhs, rhs):
+            self.lhs = lhs
+            self.rhs = rhs
+            op = custom.MatMulScaleForward(scale=self.scale)
+            return apply(op, lhs, rhs)
+
+        def backward(self, ograd):
+            op = custom.MatMulScaleBackward(scale=self.scale)
+            return apply(op, ograd, self.lhs, self.rhs)
+
+    matmul = MatMulScaleFunc(scale=0.1)
+    lhs = Tensor(np.random.randn(2, 4))
+    rhs = Tensor(np.random.randn(4, 8))
+    grad = Tensor(np.random.randn(2, 8))
+
+    with GradManager().attach([lhs, rhs]) as gm:
+        (y,) = matmul(lhs, rhs)
+        gm.backward(y)
+
+run_only_forward = False
+
+if __name__ == '__main__':
+    if run_only_forward:
+        run_forward()
+    else:
+        run_forward_and_backward()
